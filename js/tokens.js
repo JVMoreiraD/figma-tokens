628780db72b6caa129d21266018ea3e91f889c65
jvmoreirad <jvitormoreirad@hotmail.com>
2023-01-06 20:04:37 -0300

    feat: tokens

---
+++ js/tokens.js
@@ -0,0 +1,13 @@
+/**
+ * Do not edit directly
+ * Generated on Fri, 06 Jan 2023 22:58:08 GMT
+ */
+
+module.exports = {
+  "ColorWhite": "#ffffff",
+  "ColorGray400": "#c1c1c4",
+  "ColorGray500": "#50505c",
+  "ColorGray600": "#1c1c1f",
+  "ColorGray900": "#121214",
+  "ColorPurple500": "#8b5cf6"
+};
